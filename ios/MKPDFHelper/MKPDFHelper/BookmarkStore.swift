73022f50c7f5155564003d395e34902877abbad1
minkunai87-ai <minkunai87@gmail.com>
2026-08-13 10:25:48 +0900

    Use iOS-compatible folder bookmarks

--- ios/MKPDFHelper/MKPDFHelper/BookmarkStore.swift
+++ ios/MKPDFHelper/MKPDFHelper/BookmarkStore.swift
@@ -7,14 +7,14 @@ struct BookmarkStore {
     init(defaults: UserDefaults = .standard) { self.defaults = defaults }
 
     func save(folderURL: URL) throws {
-        let data = try folderURL.bookmarkData(options: [.withSecurityScope], includingResourceValuesForKeys: nil, relativeTo: nil)
+        let data = try folderURL.bookmarkData(options: [.minimalBookmark], includingResourceValuesForKeys: nil, relativeTo: nil)
         defaults.set(data, forKey: key)
     }
 
     func restore() throws -> URL? {
         guard let data = defaults.data(forKey: key) else { return nil }
         var stale = false
-        let url = try URL(resolvingBookmarkData: data, options: [.withSecurityScope], relativeTo: nil, bookmarkDataIsStale: &stale)
+        let url = try URL(resolvingBookmarkData: data, options: [.withoutUI], relativeTo: nil, bookmarkDataIsStale: &stale)
         if stale { try save(folderURL: url) }
         return url
     }
